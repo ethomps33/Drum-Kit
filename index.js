4ce70173eccf7aff21fd67c8141c364b4be92132
ethomps33 <e.thomps33@gmail.com>
2023-07-25 17:38:26 -0700

    Created Switch Statement

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
+
+
 for (var i=0 ;i<document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function() {
-    if (this.innerHTML === 's') {
-        var audio = new Audio('./sounds/kick-bass.mp3');
-        audio.play();
-    }
-    else if (this.innerHTML === 'w') {
-        var audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
+        var clickedButton = this.innerHTML;
+        switch (clickedButton) {
+            case 'w':
+                var audio = new Audio('./sounds/tom-1.mp3');
+                audio.play();
+                break;
+            case 's':
+                var audio = new Audio('./sounds/kick-bass.mp3');
+                audio.play();
+                break;
+            case 'a':
+                var audio = new Audio('./sounds/crash.mp3');
+                audio.play();
+                break;
+            case 'd':
+                var audio = new Audio('./sounds/tom-2.mp3');
+                audio.play(); 
+                break;
+            case 'j':
+                var audio = new Audio('./sounds/tom-3.mp3');
+                audio.play();
+                break;
+            case 'k':
+                var audio = new Audio('./sounds/tom-4.mp3');
+                audio.play();
+                break;
+            case 'l':
+                var audio = new Audio('./sounds/snare.mp3');
+                audio.play();
+                break;
         
-       }
-    else if (this.innerHTML === 'a') {
-        var audio = new Audio('./sounds/crash.mp3');
-        audio.play();
-    }
-    else if (this.innerHTML === 'd') {
-        var audio = new Audio('./sounds/tom-2.mp3');
-        audio.play();
-        
-       }
-    else if (this.innerHTML === 'j') {
-        var audio = new Audio('./sounds/tom-3.mp3');
-        audio.play();
-        
-       }
-    else if (this.innerHTML === 'k') {
-        var audio = new Audio('./sounds/tom-4.mp3');
-        audio.play();
-        
-       }
-    else if (this.innerHTML === 'l') {
-        var audio = new Audio('./sounds/snare.mp3');
-        audio.play();
-        
-       }
+        }
+   
     }
     );     
 }
